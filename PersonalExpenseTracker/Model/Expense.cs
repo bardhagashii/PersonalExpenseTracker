6101c140d66eb883c258a2993cde6746f209c2ba
bardhagashii <bg53539@ubt-uni.net>
2024-12-13 23:42:31 +0100

    Created Expense class and its corresponding folder

---
+++ PersonalExpenseTracker/Model/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalExpenseTracker.Model
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today; // Default to today's date
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+
+    }
+}
