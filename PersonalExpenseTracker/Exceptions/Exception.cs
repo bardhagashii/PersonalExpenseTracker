33ddc0b6135bafaf13a9b4cc407a79bf67a46ceb
bardhagashii <bg53539@ubt-uni.net>
2024-12-14 04:10:54 +0100

    Added custom exception classes for better error handling

---
+++ PersonalExpenseTracker/Exceptions/Exception.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonalExpenseTracker.Exceptions
+{
+    public class InvalidAmountException : Exception
+    {
+        public InvalidAmountException()
+            : base("Amount cannot be 0 or negative!") { }
+    }
+
+    public class EmptyDescriptionException : Exception
+    {
+        public EmptyDescriptionException()
+            : base("Description cannot be empty!") { }
+    }
+
+    public class EmptyCategoryException : Exception
+    {
+        public EmptyCategoryException()
+            : base("Category cannot be empty!") { }
+    }
+
+    public class EmptyListException : Exception
+    {
+        public EmptyListException()
+            : base("No expenses found!") { }
+    }
+
+    public class InvalidIdException : Exception
+    {
+        public InvalidIdException()
+            : base("Id cannot be 0 or negative!") { }
+    }
+
+}
